ceb07e71b52ae2685ff57581c21789a9ba80b734
KSI-YK <youhei_kunioku@kanamori-system.co.jp>
2025-05-30 16:10:04 +0900

    工程まとめ　出力機能追加

--- Utils/ProcessDetail/BuildDetail.cs
+++ Utils/ProcessDetail/BuildDetail.cs
@@ -39,7 +39,7 @@ namespace KdxDesigner.Utils.ProcessDetail
             var deviceNum = detail.Mnemonic.StartNum;
             var label = detail.Mnemonic.DeviceLabel ?? string.Empty;
 
-            // ProcessIdからデバイスを取得
+            // ProcessIdからデバイスを取得　流用する
             var process = processes.FirstOrDefault(p => p.Mnemonic.RecordId == detail.Detail.ProcessId);
             var processDeviceStartNum = process?.Mnemonic.StartNum ?? 0;
             var processDeviceLabel = process?.Mnemonic.DeviceLabel ?? string.Empty;
@@ -94,7 +94,9 @@ namespace KdxDesigner.Utils.ProcessDetail
             {
                 result.Add(LadderRow.AddLD(SettingsManager.Settings.PauseSignal));
             }
+            //
             result.Add(LadderRow.AddOR(label + (deviceNum + 0).ToString()));
+            //L3106 のところ　流用する
             result.Add(LadderRow.AddAND(processDeviceLabel + (processDeviceStartNum + 0).ToString()));
 
             foreach (var d in processDetailStartDevices)
@@ -131,7 +133,7 @@ namespace KdxDesigner.Utils.ProcessDetail
 
         // 工程まとめを出力するメソッド
         public static List<LadderCsvRow> BuildDetailSummarize(
-            MnemonicDeviceWithProcessDetail process,
+            MnemonicDeviceWithProcessDetail detail,
             List<MnemonicDeviceWithProcessDetail> details,
             List<MnemonicDeviceWithProcess> processes,
             List<MnemonicDeviceWithOperation> operations,
@@ -143,7 +145,75 @@ namespace KdxDesigner.Utils.ProcessDetail
             var result = new List<LadderCsvRow>();
             List<OutputError> localErrors = new();
 
+            // 行間ステートメントを追加
+            string id = detail.Detail.Id.ToString();
+            if (string.IsNullOrEmpty(detail.Detail.ProcessName))
+            {
+                result.Add(LadderRow.AddStatement(id));
+            }
+            else
+            {
+                result.Add(LadderRow.AddStatement(id + ":" + detail.Detail.ProcessName));
+            }
+
+            // L0 の初期値を設定　→　資料画像のL4010の事　この後末尾番号として　+9する　+0が開始で　+9が終了
+            var deviceNum = detail.Mnemonic.StartNum;
+            var label = detail.Mnemonic.DeviceLabel ?? string.Empty;
+
+            // ProcessIdからデバイスを取得
+            var process = processes.FirstOrDefault(p => p.Mnemonic.RecordId == detail.Detail.ProcessId);
+            var processDeviceStartNum = process?.Mnemonic.StartNum ?? 0;
+            var processDeviceLabel = process?.Mnemonic.DeviceLabel ?? string.Empty;// 
+
+            // ProcessDetailの開始条件
+            // この辺の処理がややこしいので共通コンポーネント化すること
+            var processDetailStartIds = detail.Detail.StartIds?.Split(';')
+                .Select(s => int.TryParse(s, out var n) ? (int?)n : null)
+                .Where(n => n.HasValue)
+                .Select(n => n.Value)
+                .ToList() ?? new List<int>();
+            var processDetailStartDevices = details
+                .Where(d => processDetailStartIds.Contains(d.Mnemonic.RecordId))
+                .ToList();
+
+
+
             // L***0 ~ L***9のDeviceリストを取得
+
+            //1行目
+            //　LD Ｍ3300　　一時停止 工程まとめでは共通　
+            result.Add(LadderRow.AddLD(SettingsManager.Settings.PauseSignal));
+            //2行目
+            //　OR　L****　 プロセス詳細開始条件　→　アウトコイルと同じ
+            result.Add(LadderRow.AddOR(label + (deviceNum + 0).ToString()));
+            //1行目
+            //L3106 のところ　流用する
+            result.Add(LadderRow.AddAND(processDeviceLabel + (processDeviceStartNum + 0).ToString()));
+
+
+            //1行目　他工程の完了確認接点 
+            foreach (var d in processDetailStartDevices)
+            {
+                result.Add(LadderRow.AddAND(d.Mnemonic.DeviceLabel + (d.Mnemonic.StartNum + 9).ToString()));
+            }
+            //1行目　OUTコイル接点　→　2行目の先頭接点と同じ　
+            result.Add(LadderRow.AddOUT(label + (deviceNum + 0).ToString()));
+
+
+            //3行目 M3300
+            result.Add(LadderRow.AddLD(SettingsManager.Settings.PauseSignal));
+
+            //4行目この工程の最終の出力番号算出と LDコマンド発行
+           
+            result.Add(LadderRow.AddOR(label + (deviceNum + 9).ToString()));
+            //3行目 M3300後の　L0初期値　AND
+            result.Add(LadderRow.AddAND(label + (deviceNum + 0).ToString()));
+
+
+            //3行目 OUT出力
+            result.Add(LadderRow.AddOUT(label + (deviceNum + 9).ToString()));
+
+
 
             // エラーをまとめて返す。
             errors.AddRange(localErrors);
